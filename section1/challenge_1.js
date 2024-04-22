a093f4f2bc2eaa7d8806b123ad58270200bfb1fc
OrregoDev <camilo.orrego554@gmail.com>
2024-04-22 15:21:51 -0500

    WIP section1/challenge_3.js

--- section1/challenge_1.js
+++ section1/challenge_1.js
@@ -1,4 +1,31 @@
 // Username generator
 
-let userName = prompt('What is your name?');
-let userLastName = prompt('What is your last name?');
+let fullName = prompt('What is your full name?');
+
+fullName = fullName.toLowerCase();
+
+let parts = fullName.split(' ');
+let firstName = parts[0].slice(0, 3);
+let lastName = parts[1].slice(0, 3);
+
+let username = firstName + lastName;
+
+let email = username + '@myDomain.com';
+
+let users = {};
+users[username] = email;
+
+let keys = Object.keys(users);
+let exists = keys.includes(username);
+
+if (exists) {
+  let i = 1;
+  while (keys.includes(username + i)) {
+    i++;
+  }
+  username += i;
+  email = username + '@myDomain.com';
+  users[username] = email;
+}
+
+console.log(users);
