fc8a19aa2d49096f38f42123b8dcd1d107f1df0e
OrregoDev <camilo.orrego554@gmail.com>
2024-04-24 15:30:52 -0500

    change section1/challenge1

--- section1/challenge_1.js
+++ section1/challenge_1.js
@@ -1,44 +1,12 @@
 // Username generator
 
-const users = {};
+const domainName = 'myDomain.com';
+const users = [];
 while (true) {
-  const fullName = prompt('Please enter your full name').toLowerCase();
-  if (!fullName) {
-    alert('Please enter your full name');
-    continue;
-  }
-  const splittedName = fullName.split(' ');
-  if (splittedName.length < 2 || splittedName.length > 2) {
-    console.log('Please enter your first name and your last name');
-    continue;
-  }
+  const fullName = prompt('Enter your full name').toLowerCase();
 
-  const initialsName = splittedName[0].slice(0, 3);
-  const initialsLastName = splittedName[1].slice(0, 3);
-  let username = `${initialsName}${initialsLastName}`;
-  let email = `${username}@myDomain.com`;
-
-  const matches = checkUnique(username);
-  if (matches > 0) {
-    username += matches;
-  }
-  console.log(username);
-  Object.assign(users, {
-    [username]: email,
-  });
-}
-
-function checkUnique(username) {
-  const userKeys = Object.keys(users);
-  let matches = 0;
-  userKeys.forEach((e) => {
-    let semiMatch = '';
-    for (let i = 0; i < e.length; i++) {
-      semiMatch += e[i];
-      if (semiMatch === username) {
-        matches++;
-      }
-    }
-  });
-  return matches;
+  let splittedName = fullName.split(' ');
+  let firstThreeFirstName = splittedName[0].slice(0, 3);
+  let firstThreeLastName = splittedName[1].slice(0, 3);
+  let username = firstThreeFirstName + firstThreeLastName;
 }
